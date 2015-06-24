4e3399afe717cc21e0304ab3f11c4a0112f83ab2
Florent Pruvost <fpruvost@inria.fr>
2015-06-24 16:37:48 +0200

    update chameleon package to get a quark variant and a non mkl variant

--- var/spack/packages/chameleon/package.py
+++ var/spack/packages/chameleon/package.py
@@ -15,16 +15,18 @@ class Chameleon(Package):
         version('0.9.1', 'fa21b7c44daf34e540ed837a9263772d')
 
     #variant('debug', default=False, description='Enable debug symbols')
+    variant('mkl', default=False, description='Use BLAS/LAPACK from the Intel MKL library')
     variant('mpi', default=False, description='Enable MPI')
     variant('cuda', default=False, description='Enable CUDA')
     variant('magma', default=False, description='Enable MAGMA kernels')
     variant('fxt', default=False, description='Enable FxT tracing support through StarPU')
     variant('simu', default=False, description='Enable simulation mode through StarPU+SimGrid')
-    variant('mkl', default=False, description='Use BLAS/LAPACK from the Intel MKL library')
+    variant('quark', default=False, description='Enable to use Quark runtime instead of StarPU')
 
     depends_on("cblas", when='~mkl')
-    depends_on("lapack", when='~mkl')
-    depends_on("starpu")
+    depends_on("lapack+lapacke", when='~mkl')
+    depends_on("starpu", when='~quark')
+    depends_on("quark", when='+quark')
     depends_on("mpi", when='+mpi')
     depends_on("magma", when='+magma')
     depends_on("fxt", when='+fxt')
@@ -56,6 +58,21 @@ class Chameleon(Package):
             if '+simu' in spec:
                 # Enable SimGrid here.
                 cmake_args.extend(["-DCHAMELEON_SIMULATION=ON"])
+            if '+quark' in spec:
+                # Enable Quark here.
+                cmake_args.extend(["-DCHAMELEON_SCHED_QUARK=ON"])
+
+            if '~mkl' in spec:
+                blas = self.spec['blas']
+                cblas = self.spec['cblas']
+                lapack = self.spec['lapack']
+                cmake_args.extend(['-DBLAS_DIR=%s' % blas.prefix])
+                cmake_args.extend(['-DCBLAS_DIR=%s' % cblas.prefix])
+                cmake_args.extend(['-DLAPACK_DIR=%s' % lapack.prefix])
+                cmake_args.extend(['-DLAPACKE_DIR=%s' % lapack.prefix])
+                cmake_args.extend(['-DTMG_DIR=%s' % lapack.prefix])
+                if "%gcc" in spec:
+                    os.environ["LDFLAGS"] = "-lgfortran"
 
             cmake_args.extend(std_cmake_args)
 
