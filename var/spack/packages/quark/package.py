c8610c51fb611a1e8aa3f5df261dd509b824fea2
Florent Pruvost <fpruvost@inria.fr>
2015-06-24 16:35:59 +0200

    add quark package

---
+++ var/spack/packages/quark/package.py
@@ -0,0 +1,14 @@
+from spack import *
+
+class Quark(Package):
+    """Enables the dynamic execution of tasks with data dependencies in a multi-core, multi-socket, shared-memory environment."""
+    homepage = "http://icl.cs.utk.edu/quark/index.html"
+    url      = "http://icl.cs.utk.edu/projectsfiles/quark/pubs/quark-0.9.0.tgz"
+
+    version('0.9.0', '52066a24b21c390d2f4fb3b57e976d08')
+
+    def install(self, spec, prefix):
+        mf = FileFilter('make.inc')
+        mf.filter('prefix=./install', 'prefix=%s' % prefix)
+        make()
+        make("install")
