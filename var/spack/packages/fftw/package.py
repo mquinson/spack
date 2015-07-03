b792531794f1e65b3fa56bf9b02143abbac9d250
Florent Pruvost <fpruvost@inria.fr>
2015-07-03 19:43:48 +0200

    add fftw package

---
+++ var/spack/packages/fftw/package.py
@@ -0,0 +1,33 @@
+from spack import *
+
+class Fftw(Package):
+    """a C subroutine library for computing the discrete Fourier transform (DFT) in one or more dimensions, of arbitrary input size, and of both real and complex data."""
+    homepage = "http://www.fftw.org/"
+    url      = "http://www.fftw.org/fftw-3.3.4.tar.gz"
+
+    version('3.3.4', '2edab8c06b24feeb3b82bbb3ebf3e7b3')
+
+    variant('mpi', default=False, description='Enable MPI')
+    variant('openmp', default=False, description='Enable openmp')
+    variant('threads', default=False, description='Enable threads')
+
+    depends_on("mpi", when='+mpi')
+
+    def install(self, spec, prefix):
+
+        config_args = ["--prefix=" + prefix]
+        config_args.append("--enable-shared")
+        config_args.append("--enable-float")
+
+        if '+mpi' in spec:
+            config_args.append("--enable-mpi")
+
+        if '+openmp' in spec:
+            config_args.append("--enable-openmp")
+
+        if '+threads' in spec:
+            config_args.append("--enable-threads")
+
+        configure(*config_args)
+        make()
+        make("install")
