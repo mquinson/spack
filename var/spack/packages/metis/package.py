b1af5ca8dcafac4d4486cb95d1156e93e0d26d03
Florent Pruvost <fpruvost@inria.fr>
2015-09-01 15:28:19 +0200

    add metis 4.0.3 version

--- var/spack/packages/metis/package.py
+++ var/spack/packages/metis/package.py
@@ -1,4 +1,5 @@
 from spack import *
+import os
 
 class Metis(Package):
     """METIS is a set of serial programs for partitioning graphs,
@@ -11,17 +12,33 @@ class Metis(Package):
     url      = "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz"
 
     version('5.1.0', '5465e67079419a69e0116de24fce58fe')
+    version('4.0.3', 'd3848b454532ef18dc83e4fb160d1e10',
+            url='http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz')
 
-    depends_on('mpi')
+    depends_on('mpi', when='@5:')
 
     def install(self, spec, prefix):
-        cmake(".",
-              '-DGKLIB_PATH=%s/GKlib' % pwd(),
-              '-DSHARED=1',
-              '-DCMAKE_C_COMPILER=mpicc',
-              '-DCMAKE_CXX_COMPILER=mpicxx',
-              '-DSHARED=1',
-              *std_cmake_args)
+        if spec.satisfies('@5:'):
+            cmake(".",
+                  '-DGKLIB_PATH=%s/GKlib' % pwd(),
+                  '-DCMAKE_C_COMPILER=mpicc',
+                  '-DCMAKE_CXX_COMPILER=mpicxx',
+                  '-DSHARED=1',
+                  *std_cmake_args)
 
-        make()
-        make("install")
+            make()
+            make("install")
+        else:
+            make()
+
+            # No install provided
+            mkdirp('%s/Lib' % prefix)
+            mkdirp(prefix.include)
+            with working_dir('Lib'):
+                for file in os.listdir("%s" % self.stage.path + "/metis-%s/Lib" % spec.version):
+                    if file.endswith(".h"):
+                        install(file, '%s/Lib' % prefix)
+                        install(file, prefix.include)
+
+            mkdirp(prefix.lib)
+            install('libmetis.a', prefix.lib)
