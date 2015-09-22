5abc1bf316617513533b97ea9bf889c239e912b8
Florent Pruvost <fpruvost@inria.fr>
2015-09-22 13:18:53 +0200

    add hwloc dependency to quark

--- var/spack/packages/quark/package.py
+++ var/spack/packages/quark/package.py
@@ -7,6 +7,8 @@ class Quark(Package):
 
     version('0.9.0', '52066a24b21c390d2f4fb3b57e976d08')
 
+    depends_on("hwloc")
+
     def install(self, spec, prefix):
         mf = FileFilter('make.inc')
         mf.filter('prefix=./install', 'prefix=%s' % prefix)
