99192f2f4455508c01927fafde870882e039ea59
Florent Pruvost <fpruvost@inria.fr>
2015-09-25 10:53:56 +0200

    pastix can work with metis version < 5

--- var/spack/packages/pastix/package.py
+++ var/spack/packages/pastix/package.py
@@ -23,7 +23,7 @@ class Pastix(Package):
     depends_on("mpi", when='+mpi')
     depends_on("blas", when='~mkl')
     depends_on("scotch")
-    depends_on("metis", when='+metis')
+    depends_on("metis@4.0.3", when='+metis')
     depends_on("starpu@1.1.0:1.1.5", when='+starpu')
 
     def patch(self):
