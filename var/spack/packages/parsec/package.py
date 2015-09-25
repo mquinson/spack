d72f46fe2e020b2c7466aef3d187781c0114afcd
Florent Pruvost <fpruvost@inria.fr>
2015-09-25 17:33:01 +0200

    add parsec package

---
+++ var/spack/packages/parsec/package.py
@@ -0,0 +1,20 @@
+from spack import *
+
+class Parsec(Package):
+    """Parallel Runtime Scheduling and Execution Controller."""
+    homepage = "http://icl.cs.utk.edu/parsec/index.html"
+
+    version('last-rel', '3c09cc8bd413a435254d6cc0e2e5f663',
+            url='http://icl.cs.utk.edu/projectsfiles/parsec/pubs/parsec-b9861d3a818f.tgz')
+
+    depends_on("hwloc")
+    depends_on("mpi")
+    depends_on("papi")
+
+    def install(self, spec, prefix):
+        cmake_args = ["-DBUILD_SHARED_LIBS=ON"]
+        cmake_args.extend(std_cmake_args)
+
+        cmake(*cmake_args)
+        make()
+        make("install")
