515158063689bdee83188e4fc9260b35301cfe56
Aurélien Falco <aurelien.falco@inria.fr>
2015-10-23 15:59:59 +0200

    blacs install properly

--- var/spack/packages/blacs/package.py
+++ var/spack/packages/blacs/package.py
@@ -8,23 +8,30 @@ class Blacs(Package):
     homepage = "http://www.netlib.org/blacs/"
 
     # tarball has no version, but on the date below, this MD5 was correct.
-    # version('1997-05-05', '28ae5b91b3193402fe1ae8d06adcf500', url='http://www.netlib.org/blacs/mpiblacs.tgz')
+    version('1997-05-05', '28ae5b91b3193402fe1ae8d06adcf500', url='http://www.netlib.org/blacs/mpiblacs.tgz')
     version('lib','82687f1e07fd98e0b9f78b71911459fe', url='http://www.netlib.org/blacs/archives/blacs_MPI-LINUX-0.tgz')
-    # depends_on('mpich')
-
-    # def patch(self):
-    	# os.symlink('BMAKES/Bmake.PVM-LINUX', 'Bmake.inc')
-        # mf = FileFilter('Bmake.inc')
-
-        # mf.filter('MPIdir = /usr/local/mpich', 'MPIdir = %s' % self.spec['mpich'].prefix)
-        # mf.filter('/bin/sh', '/bin/bash')
+    depends_on('openmpi')
 
     def install(self, spec, prefix):
+        call(['cp', 'BMAKES/Bmake.MPI-LINUX', 'Bmake.inc'])
+        mf = FileFilter('Bmake.inc')
+
+        mf.filter('\$\(HOME\)/BLACS', '%s' % os.getcwd())
+        mf.filter('/usr/local/mpich', '%s' % self.spec['openmpi'].prefix)
+        mf.filter('\$\(MPILIBdir\)/libmpich\.a', '')
+        mf.filter('\$\(MPIdir\)/lib/', '')
+        mf.filter('DUseMpich', 'DUseMpi2')
+        mf.filter('F77            = g77', 'F77            = mpif77')
+        mf.filter('CC             = gcc', 'CC             = mpicc')
+        filter_file('\$\(MAKE\) -f \.\./Makefile I_int \"dlvl=\$\(BTOPdir\)\" \)','echo $(BLACSDEFS) $(MAKE) -f ../Makefile I_int "dlvl=$(BTOPdir)" )', 'SRC/MPI/Makefile')
+        call(['cat', 'Bmake.inc'])
+        call(['cat', 'SRC/MPI/Makefile'])
+
+        make('mpi')
         mkdirp(prefix.lib)
-        call(['ls'])
-        install('./blacsCinit_MPI-LINUX-0.a', '%s/libblacsCinit-openmpi.a' % prefix.lib)
-        install('./blacsF77init_MPI-LINUX-0.a', '%s/libblacsF77init-openmpi.a' % prefix.lib)
-        install('./blacs_MPI-LINUX-0.a', '%s/libblacs-openmpi.a' % prefix.lib)
+        install('LIB/blacsCinit_MPI-LINUX-0.a', '%s/libblacsCinit-openmpi.a' % prefix.lib)
+        install('LIB/blacsF77init_MPI-LINUX-0.a', '%s/libblacsF77init-openmpi.a' % prefix.lib)
+        install('LIB/blacs_MPI-LINUX-0.a', '%s/libblacs-openmpi.a' % prefix.lib)
 
         	    
 
