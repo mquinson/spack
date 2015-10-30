a511cf418efd86d4ea53193f86b0944d399640d2
SYLVAND Guillaume <guillaume.sylvand@airbus.com>
2015-10-30 11:14:32 +0100

    package blacs: now depends on mpi + shared variant

--- var/spack/packages/blacs/package.py
+++ var/spack/packages/blacs/package.py
@@ -10,29 +10,44 @@ class Blacs(Package):
     # tarball has no version, but on the date below, this MD5 was correct.
     version('1997-05-05', '28ae5b91b3193402fe1ae8d06adcf500', url='http://www.netlib.org/blacs/mpiblacs.tgz')
     version('lib','82687f1e07fd98e0b9f78b71911459fe', url='http://www.netlib.org/blacs/archives/blacs_MPI-LINUX-0.tgz')
-    depends_on('openmpi')
+    depends_on('mpi')
+
+    variant('shared', default=True, description='Build BLACS as a shared library')
 
     def install(self, spec, prefix):
         call(['cp', 'BMAKES/Bmake.MPI-LINUX', 'Bmake.inc'])
         mf = FileFilter('Bmake.inc')
 
         mf.filter('\$\(HOME\)/BLACS', '%s' % os.getcwd())
-        mf.filter('/usr/local/mpich', '%s' % self.spec['openmpi'].prefix)
+        mf.filter('/usr/local/mpich', '%s' % self.spec['mpi'].prefix)
         mf.filter('\$\(MPILIBdir\)/libmpich\.a', '')
         mf.filter('\$\(MPIdir\)/lib/', '')
         mf.filter('DUseMpich', 'DUseMpi2')
         mf.filter('F77            = g77', 'F77            = mpif77')
         mf.filter('CC             = gcc', 'CC             = mpicc')
+
+        if spec.satisfies('+shared'):
+            mf.filter('ARCH\s*=.*', 'ARCH=$(CC)')
+            mf.filter('ARCHFLAGS\s*=.*', 'ARCHFLAGS=-shared -o')
+            mf.filter('RANLIB\s*=.*', 'RANLIB=echo')
+            mf.filter('CCFLAGS\s*=', 'CCFLAGS = -fPIC ')
+            mf.filter('F77FLAGS\s*=', 'F77FLAGS = -fPIC ')
+            mf.filter('\.a', '.so')
+
         filter_file('\$\(MAKE\) -f \.\./Makefile I_int \"dlvl=\$\(BTOPdir\)\" \)','echo $(BLACSDEFS) $(MAKE) -f ../Makefile I_int "dlvl=$(BTOPdir)" )', 'SRC/MPI/Makefile')
         call(['cat', 'Bmake.inc'])
         call(['cat', 'SRC/MPI/Makefile'])
 
         make('mpi')
         mkdirp(prefix.lib)
-        install('LIB/blacsCinit_MPI-LINUX-0.a', '%s/libblacsCinit-openmpi.a' % prefix.lib)
-        install('LIB/blacsF77init_MPI-LINUX-0.a', '%s/libblacsF77init-openmpi.a' % prefix.lib)
-        install('LIB/blacs_MPI-LINUX-0.a', '%s/libblacs-openmpi.a' % prefix.lib)
-
+        if spec.satisfies('+shared'):
+            install('LIB/blacsCinit_MPI-LINUX-0.so', '%s/libblacsCinit.so' % prefix.lib)
+            install('LIB/blacsF77init_MPI-LINUX-0.so', '%s/libblacsF77init.so' % prefix.lib)
+            install('LIB/blacs_MPI-LINUX-0.so', '%s/libblacs.so' % prefix.lib)
+        else:
+            install('LIB/blacsCinit_MPI-LINUX-0.a', '%s/libblacsCinit.a' % prefix.lib)
+            install('LIB/blacsF77init_MPI-LINUX-0.a', '%s/libblacsF77init.a' % prefix.lib)
+            install('LIB/blacs_MPI-LINUX-0.a', '%s/libblacs.a' % prefix.lib)
         	    
 
 
