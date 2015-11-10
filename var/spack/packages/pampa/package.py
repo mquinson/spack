6357696e3702d493c5d1678f4a878d8287dc6545
Nikolas Pattakos <npattako@inria.fr>
2015-11-10 16:09:43 +0100

    PAMPA_USERNAME environment variable
    Look for such an environment variable to determine which is the user's
    gforge username.
    If this is not set, fall back to using the current user's login name.

--- var/spack/packages/pampa/package.py
+++ var/spack/packages/pampa/package.py
@@ -1,15 +1,25 @@
 from spack import *
+import os
 import getpass
 
 class Pampa(Package):
     """PaMPA is a middleware that manages distributed meshes. It allows
        users to write parallel solver codes without having to bother about
-       data exchange, data redistribution, remeshing and load balancing"""
+       data exchange, data redistribution, remeshing and load balancing.
+       As there is not yet a public release of the project I need to connect
+       to the svn repository using your gforge account - this is assumed to
+       be the same as your shell's username, if this is not correct please
+       set environment variable PAMPA_USERNAME to your gforge account name."""
 
     homepage = "http://gforge.inria.fr/projects/pampa-p/"
     url      = "http://www.example.com/pampa-1.0.tar.gz"
 
-    version('svn-head', svn='https://scm.gforge.inria.fr/authscm/'+getpass.getuser()+'/svn/pampa-p/trunk')
+    try:
+        username = os.environ['PAMPA_USERNAME']
+    except KeyError:
+        username = getpass.getuser()
+
+    version('svn-head', svn='https://scm.gforge.inria.fr/authscm/' + username + '/svn/pampa-p/trunk')
 
     depends_on('mpi')
     depends_on('scotch@6.0.0:6.0.3 +mpi ~pthread')
