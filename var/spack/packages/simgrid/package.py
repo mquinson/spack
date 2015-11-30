b1297cfaac82bada810a49f586643dbacb10eb98
Florent Pruvost <fpruvost@inria.fr>
2015-11-30 10:34:24 +0100

    add new version of simgrid 3.12

--- var/spack/packages/simgrid/package.py
+++ var/spack/packages/simgrid/package.py
@@ -3,12 +3,15 @@ from spack import *
 class Simgrid(Package):
     """To study the behavior of large-scale distributed systems such as Grids, Clouds, HPC or P2P systems."""
     homepage = "http://simgrid.gforge.inria.fr/index.html"
-    url      = "http://gforge.inria.fr/frs/download.php/file/33686/SimGrid-3.11.1.tar.gz"
+    url      = "http://gforge.inria.fr/frs/download.php/file/35215/SimGrid-3.12.tar.gz"
 
-    version('3.11.1', 'eb15a815a609bc8784ed4d55baafe435')
-    version('3.11', '358ed81042bd283348604eb1beb80224')
-    version('3.10', 'a345ad07e37539d54390f817b7271de7')
-    version('3.10', '2aca712d073fdd21a86dd2fcf1aabf12')
+
+    version('3.12', 'd73faaf81d7a9eb0d309cfd72532c5f1',
+            url='http://gforge.inria.fr/frs/download.php/file/35215/SimGrid-3.12.tar.gz')
+    version('3.11', '358ed81042bd283348604eb1beb80224',
+            url='http://gforge.inria.fr/frs/download.php/file/33683/SimGrid-3.11.tar.gz')
+    version('3.10', 'a345ad07e37539d54390f817b7271de7',
+            url='http://gforge.inria.fr/frs/download.php/file/33124/SimGrid-3.10.tar.gz')
 
     def install(self, spec, prefix):
         cmake('.', *std_cmake_args)
