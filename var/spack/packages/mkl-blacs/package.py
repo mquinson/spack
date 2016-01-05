adb40e28d7ca0fb884d27216590c256722ed4d70
SYLVAND Guillaume <guillaume.sylvand@airbus.com>
2016-01-05 15:33:06 +0100

    mkl-scalapack, mkl-blacs: disable it on OSX (where MKL doesnt provide it)

--- var/spack/packages/mkl-blacs/package.py
+++ var/spack/packages/mkl-blacs/package.py
@@ -2,6 +2,7 @@ from spack import *
 import os
 import spack
 import sys
+import platform
 
 class MklBlacs(Package):
     """Intel MKL Blas, Lapack, Scalapack routines"""
@@ -13,34 +14,29 @@ class MklBlacs(Package):
     version('exist', '7b878b76545ef9ddb6f2b61d4c4be833',
             url = "file:"+join_path(pkg_dir, "empty.tar.gz"))
 
-    # virtual dependency
-    if os.getenv('MKLROOT'):
+    # virtual dependency (no blacs/scalapack in OSX version of MKL)
+    if platform.system() != "Darwin" and os.getenv('MKLROOT'):
         mklroot=os.environ['MKLROOT']
         if os.path.isdir(mklroot):
             provides('blacs')
 
-    variant('mt', default=False, description="Use Multithreaded version")
     variant('shared', default=True, description="Use shared library version")
 
     depends_on("mpi")
 
     def setup_dependent_environment(self, module, spec, dep_spec):
-
-        if os.getenv('MKLROOT'):
-            mklroot=os.environ['MKLROOT']
-            if os.path.isdir(mklroot):
-                """Dependencies of this package will get the libraries names for mkl-blacs."""
-                mkllibdir=mklroot+"/lib/intel64/"
-                if spec.satisfies('+shared'):
-                    if spec.satisfies('%gcc'):
-                        module.blacslibname=['-Wl,--no-as-needed', ' -L'+mkllibdir, ' -lmkl_blacs_intelmpi_lp64']
-                    else:
-                        module.blacslibname=['-L'+mkllibdir, ' -lmkl_blacs_intelmpi_lp64']
-                else:
-                    if '^intelmpi' in spec or '^mpich' in spec:
-                        module.blacslibname=mkllibdir+'libmkl_blacs_intelmpi_lp64.a'
-                    elif '^openmpi' in spec:
-                        module.blacslibname=mkllibdir+'libmkl_blacs_openmpi_lp64.a'
+        """Dependencies of this package will get the libraries names for mkl-blacs."""
+        mkllibdir=self.spec.prefix.lib
+        if spec.satisfies('+shared'):
+            if spec.satisfies('%gcc'):
+                module.blacslibname=["-Wl,--no-as-needed ", '-L'+mkllibdir, ' -lmkl_blacs_intelmpi_lp64']
+            else:
+                module.blacslibname=['-L'+mkllibdir, ' -lmkl_blacs_intelmpi_lp64']
+        else:
+            if '^intelmpi' in spec or '^mpich' in spec:
+                module.blacslibname=[mkllibdir+'/libmkl_blacs_intelmpi_lp64.a']
+            elif '^openmpi' in spec:
+                module.blacslibname=[mkllibdir+'/libmkl_blacs_openmpi_lp64.a']
 
     def install(self, spec, prefix):
         if spec.satisfies('+shared') and not '^intelmpi' in spec and not '^mpich' in spec:
@@ -50,7 +46,6 @@ class MklBlacs(Package):
         if os.getenv('MKLROOT'):
             mklroot=os.environ['MKLROOT']
             if os.path.isdir(mklroot):
-                mklroot=os.environ['MKLROOT']
                 os.symlink(mklroot+"/bin", prefix.bin)
                 os.symlink(mklroot+"/include", prefix.include)
                 os.symlink(mklroot+"/lib/intel64", prefix.lib)
