5d10e39b3c3851e78d8d067e846a776effa040ea
SYLVAND Guillaume <guillaume.sylvand@airbus.com>
2016-01-21 18:54:27 +0100

    package flex: add version 'exist', and disable version 2.5.39 on OSX (doesnt compile)

--- var/spack/packages/flex/package.py
+++ var/spack/packages/flex/package.py
@@ -1,4 +1,6 @@
 from spack import *
+import spack
+import os, platform
 
 class Flex(Package):
     """Flex is a tool for generating scanners."""
@@ -6,10 +8,22 @@ class Flex(Package):
     homepage = "http://flex.sourceforge.net/"
     url      = "http://download.sourceforge.net/flex/flex-2.5.39.tar.gz"
 
-    version('2.5.39', 'e133e9ead8ec0a58d81166b461244fde')
+    # For a reason I dont want to investigate, flex doesnt compile on OSX with macports
+    if not platform.system() == 'Darwin':
+        version('2.5.39', 'e133e9ead8ec0a58d81166b461244fde', url = "http://download.sourceforge.net/flex/flex-2.5.39.tar.gz")
+
+    pkg_dir = spack.db.dirname_for_package_name("flex")
+    # fake tarball because we consider it is already installed
+    version('exist', '7b878b76545ef9ddb6f2b61d4c4be833',
+            url = "file:"+join_path(pkg_dir, "empty.tar.gz"))
 
     def install(self, spec, prefix):
         configure("--prefix=%s" % prefix)
 
         make()
         make("install")
+
+    # to use the existing version available in the environment
+    @when('@exist')
+    def install(self, spec, prefix):
+        os.symlink("/usr/bin", prefix.bin)
