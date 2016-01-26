7d4435a4e2f087e789c4ebd09953692d37f66794
SYLVAND Guillaume <guillaume.sylvand@airbus.com>
2016-01-26 13:47:30 +0100

    hmat package: 2 corrections to repair

--- var/spack/packages/hmat/package.py
+++ var/spack/packages/hmat/package.py
@@ -11,6 +11,7 @@ class Hmat(Package):
     Set the environment variable SOFTWARREEPO1 to get the versions.
     """
     homepage = "http://www.google.com"
+    pkg_dir = spack.db.dirname_for_package_name("mpf")
 
     try:
         repo=os.environ['SOFTWAREREPO1']
@@ -104,7 +105,7 @@ class Hmat(Package):
             cmake_args.extend(["-DUSE_DEBIAN_OPENBLAS=OFF"])
 
             if platform.system() == 'Darwin':
-                filter_file('_LINK_HMAT_OSS LINK_PRIVATE.*', '_LINK_HMAT_OSS LINK_PRIVATE -Wl,-force_load,${_HMAT_OSS_PATH}  ${hmat-oss_LIB_DEPENDS})', '../CMakeLists.txt')
+                filter_file('_LINK_HMAT_OSS LINK_PRIVATE.*', '_LINK_HMAT_OSS LINK_PRIVATE -Wl,-force_load,${_HMAT_OSS_PATH}  ${hmat-oss_LIB_DEPENDS})', project_dir+'/CMakeLists.txt')
 
             cmake(*cmake_args)
 
