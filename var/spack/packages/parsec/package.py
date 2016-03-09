d8e73be710d329e4e416e14407fe9496af631afb
matias hastaran <matias.hastaran@inria.fr>
2016-03-09 17:46:49 +0100

    add new variant in parsec

--- var/spack/packages/parsec/package.py
+++ var/spack/packages/parsec/package.py
@@ -7,15 +7,19 @@ class Parsec(Package):
 
     version('last-rel', '3c09cc8bd413a435254d6cc0e2e5f663',
             url='http://icl.cs.utk.edu/projectsfiles/parsec/pubs/parsec-b9861d3a818f.tgz')
-
+    version ('master', git='https://bitbucket.org/icldistcomp/parsec.git', branch='master')
     pkg_dir = spack.db.dirname_for_package_name("fake")
     # fake tarball because we consider it is already installed
     version('exist', '7b878b76545ef9ddb6f2b61d4c4be833',
             url = "file:"+join_path(pkg_dir, "empty.tar.gz"))
     version('src')
 
+
     variant('mpi', default=True, description='Enable MPI support')
     variant('papi', default=False, description='Enable PAPI support for using PINS')
+    variant('shared', default=False, description='Enable shared library')
+    variant('int64', default=False, description='Enable 64bits')
+    variant('dplasma', default=False, description='Enable DPlasma')
 
     depends_on("cmake")
     depends_on("hwloc")
@@ -23,9 +27,23 @@ class Parsec(Package):
     depends_on("papi", when="+papi")
 
     def install(self, spec, prefix):
-        cmake_args = ["-DBUILD_SHARED_LIBS=ON"]
-        cmake_args.extend(std_cmake_args)
+        if spec.satisfies('+shared'):
+            cmake_args = ["-DBUILD_SHARED_LIBS=ON"]
+        else:
+            cmake_args = ["-DBUILD_SHARED_LIBS=OFF"]
 
+        if spec.satisfies('+int64'):
+            cmake_args = ["-DBUILD_64bits=ON"]
+        else:
+            cmake_args = ["-DBUILD_64bits=OFF"]
+
+        if spec.satisfies('+dplasma'):
+            cmake_args = ["-DBUILD_DPLASMA=ON"]
+        else:
+            cmake_args = ["-DBUILD_DPLASMA=OFF"]
+
+        cmake.args = ['-DDAGUE_WITH_DEVEL_HEADERS=ON']
+        cmake_args.extend(std_cmake_args)
         cmake(*cmake_args)
         make()
         make("install")
