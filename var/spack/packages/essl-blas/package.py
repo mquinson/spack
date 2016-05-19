eebaa1740e06a2d1d53846f921bde050a9fc2d61
Florent Pruvost <florent.pruvost@inria.fr>
2016-05-19 12:54:24 +0200

    make essl build netlib because essl lib does not provide all blas/lapack symbols

--- var/spack/packages/essl-blas/package.py
+++ var/spack/packages/essl-blas/package.py
@@ -8,36 +8,86 @@ class EsslBlas(Package):
     """IBM ESSL Blas and Lapack routines"""
     homepage = "http://www-03.ibm.com/systems/power/software/essl/"
 
-    pkg_dir = spack.db.dirname_for_package_name("fake")
+    # essl does not provide all blas symbols... so that we also install a netlib-blas
+    version('with-netlib', 'f2f6c67134e851fe189bb3ca1fbb5101',
+            url="http://www.netlib.org/lapack/lapack-3.6.0.tgz")
 
+    pkg_dir = spack.db.dirname_for_package_name("fake")
     # fake tarball because we consider it is already installed
     version('exist', '7b878b76545ef9ddb6f2b61d4c4be833',
             url = "file:"+join_path(pkg_dir, "empty.tar.gz"))
 
     # virtual dependency
-    if os.getenv('ESSLROOT'):
+    if os.getenv('ESSLROOT') and os.getenv('XLFROOT') and os.getenv('XLSMPROOT'):
         esslroot=os.environ['ESSLROOT']
-        if os.path.isdir(esslroot):
+        xlfroot=os.environ['XLFROOT']
+        xlsmproot=os.environ['XLSMPROOT']
+        if os.path.isdir(esslroot) and os.path.isdir(xlfroot) and os.path.isdir(xlsmproot):
             provides('blas')
 
     variant('mt', default=False, description="Use Multithreaded version")
 
     def setup_dependent_environment(self, module, spec, dep_spec):
         """Dependencies of this package will get the libraries names for essl-blas."""
+
+        if spec.satisfies('@exist'):
+            netlib_blas_libs = ""
+        else:
+            # set netlib-blas lib
+            if os.path.isdir(spec.prefix.lib64):
+                libdir = self.spec.prefix+"/lib64"
+            if os.path.isdir(spec.prefix.lib):
+                libdir = self.spec.prefix+"/lib"
+            netlib_blas_libs = "-L%s -lblas" % libdir
+
+        # set essl lib
         esslroot=os.environ['ESSLROOT']
-        if os.path.isdir(esslroot):
+        xlfroot=os.environ['XLFROOT']
+        if os.path.isdir(esslroot) and os.path.isdir(xlfroot):
             if spec.satisfies("+mt"):
                 xlsmproot=os.environ['XLSMPROOT']
-                if os.path.isdir(xlfroot):
-                    module.blaslibname=["-Wl,--unresolved-symbols=ignore-in-shared-libs -L%s/lib -R%s/lib -lesslsmp -L%s/lib -lxlsmp" %(esslroot,esslroot,xlsmproot)]
+                if os.path.isdir(xlsmproot):
+                    module.blaslibname=["-L%s -R%s -lesslsmp -L%s -lxlsmp -L%s -lxlfmath -lxlf90 -lxlf90_r %s -lesslsmp -lxlsmp -lxlfmath -lxlf90 -lxlf90_r" %(esslroot,esslroot,xlsmproot,xlfroot,netlib_blas_libs)]
                 else:
                     sys.exit('XLSMPROOT environment variable does not exist. Please set XLSMPROOT, where lies libxlsmp, to use the ESSL Blas')
             else:
-                module.blaslibname=["-Wl,--unresolved-symbols=ignore-in-shared-libs -L%s/lib -R%s/lib -lessl" % (esslroot,esslroot)]
+                module.blaslibname=["-L%s -R%s -lessl -L%s -lxlfmath -lxlf90 -lxlf90_r %s -lxlfmath -lxlf90 -lxlf90_r" % (esslroot,esslroot,xlfroot,netlib_blas_libs)]
             module.blaslibfortname=module.blaslibname
         else:
-            sys.exit('ESSLROOT environment variable does not exist. Please set ESSLROOT, where lies libessl, to use the ESSL Blas')
+            sys.exit('ESSLROOT or XLFROOT environment variable does not exist. Please set ESSLROOT and XLFROOT, where lies libessl and libxlf90, to use the ESSL Blas')
 
+    # Null literal string is not permitted with xlf
+    def patch_xlf(self):
+
+        mf = FileFilter('BLAS/SRC/xerbla_array.f')
+        mf.filter('SRNAME = \'\'', 'SRNAME = \' \'')
+
+    def install(self, spec, prefix):
+
+        if spec.satisfies('%xl'):
+            self.patch_xlf()
+
+        # Disable the building of lapack in CMakeLists.txt
+        mf = FileFilter('CMakeLists.txt')
+        mf.filter('add_subdirectory\(SRC\)','#add_subdirectory(SRC)')
+        mf.filter('set\(ALL_TARGETS \$\{ALL_TARGETS\} lapack\)','#set(ALL_TARGETS ${ALL_TARGETS} lapack)')
+
+        cmake_args = ["."]
+        cmake_args+= std_cmake_args
+        cmake_args+=[
+                "-DBUILD_TESTING=OFF",
+                "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"]
+        cmake_args.append('-DBUILD_SHARED_LIBS=ON')
+        cmake_args.append('-DBUILD_STATIC_LIBS=OFF')
+        cmake_args.append('-DCMAKE_INSTALL_LIBDIR=lib')
+        if platform.system() == 'Darwin':
+            cmake_args.append('-DCMAKE_SHARED_LINKER_FLAGS=-undefined dynamic_lookup')
+
+        cmake(*cmake_args)
+        make()
+        make("install")
+
+    @when('@exist')
     def install(self, spec, prefix):
         if os.getenv('ESSLROOT'):
             esslroot=os.environ['ESSLROOT']
--- var/spack/packages/essl-lapack/package.py
+++ var/spack/packages/essl-lapack/package.py
@@ -8,6 +8,9 @@ class EsslLapack(Package):
     """IBM ESSL Blas and Lapack routines"""
     homepage = "http://www-03.ibm.com/systems/power/software/essl/"
 
+    version('with-netlib', 'f2f6c67134e851fe189bb3ca1fbb5101',
+            url="http://www.netlib.org/lapack/lapack-3.6.0.tgz")
+
     pkg_dir = spack.db.dirname_for_package_name("fake")
 
     # fake tarball because we consider it is already installed
@@ -15,9 +18,11 @@ class EsslLapack(Package):
             url = "file:"+join_path(pkg_dir, "empty.tar.gz"))
 
     # virtual dependency
-    if os.getenv('ESSLROOT') :
+    if os.getenv('ESSLROOT') and os.getenv('XLFROOT') and os.getenv('XLSMPROOT'):
         esslroot=os.environ['ESSLROOT']
-        if os.path.isdir(esslroot):
+        xlfroot=os.environ['XLFROOT']
+        xlsmproot=os.environ['XLSMPROOT']
+        if os.path.isdir(esslroot) and os.path.isdir(xlfroot) and os.path.isdir(xlsmproot):
             provides('lapack')
 
     variant('mt', default=False, description="Use Multithreaded version")
@@ -25,22 +30,66 @@ class EsslLapack(Package):
     # blas is a virtual dependency.
     depends_on('blas')
 
+    depends_on('cmake')
+
     def setup_dependent_environment(self, module, spec, dep_spec):
         """Dependencies of this package will get the libraries names for essl-lapack."""
+
+        if spec.satisfies('@exist'):
+            netlib_lapack_libs = ""
+        else:
+            # set netlib-lapack lib
+            if os.path.isdir(spec.prefix.lib64):
+                libdir = self.spec.prefix+"/lib64"
+            if os.path.isdir(spec.prefix.lib):
+                libdir = self.spec.prefix+"/lib"
+            netlib_lapack_libs = "-L%s -llapack" % libdir
+
+        # set essl lib
         esslroot=os.environ['ESSLROOT']
-        if os.path.isdir(esslroot):
+        xlfroot=os.environ['XLFROOT']
+        if os.path.isdir(esslroot) and os.path.isdir(xlfroot):
             if spec.satisfies("+mt"):
                 xlsmproot=os.environ['XLSMPROOT']
                 if os.path.isdir(xlfroot):
-                    module.lapacklibname=["-Wl,--unresolved-symbols=ignore-in-shared-libs -L%s/lib -R%s/lib -lesslsmp -L%s/lib -lxlsmp" %(esslroot,esslroot,xlsmproot)]
+                    module.lapacklibname=["-L%s -R%s -lesslsmp -L%s -lxlsmp -L%s -lxlfmath -lxlf90 -lxlf90_r %s -lesslsmp -lxlsmp -lxlfmath -lxlf90 -lxlf90_r" %(esslroot,esslroot,xlsmproot,xlfroot,netlib_lapack_libs)]
                 else:
                     sys.exit('XLSMPROOT environment variable does not exist. Please set XLSMPROOT, where lies libxlsmp, to use the ESSL LAPACK')
             else:
-                module.lapacklibname=["-Wl,--unresolved-symbols=ignore-in-shared-libs -L%s/lib -R%s/lib -lessl" % (esslroot,esslroot)]
+                module.lapacklibname=["-L%s -R%s -lessl -L%s -lxlfmath -lxlf90 -lxlf90_r %s -lxlfmath -lxlf90 -lxlf90_r" % (esslroot,esslroot,xlfroot,netlib_lapack_libs)]
             module.lapacklibfortname=module.lapacklibname
         else:
-            sys.exit('ESSLROOT environment variable does not exist. Please set ESSLROOT, where lies libessl, to use the ESSL LAPACK')
+            sys.exit('ESSLROOT or XLFROOT environment variable does not exist. Please set ESSLROOT and XLFROOT, where lies libessl and libxlf90, to use the ESSL Blas')
 
+    # Null literal string is not permitted with xlf
+    def patch_xlf(self):
+
+        mf = FileFilter('SRC/xerbla_array.f')
+        mf.filter('SRNAME = \'\'', 'SRNAME = \' \'')
+
+    def install(self, spec, prefix):
+
+        if spec.satisfies('%xl'):
+            self.patch_xlf()
+
+        cmake_args = ["."]
+        cmake_args += std_cmake_args
+        cmake_args += ["-Wno-dev", "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"]
+
+        blas_libs = " ".join(blaslibfortname)
+        blas_libs = blas_libs.replace(' ', ';')
+        cmake_args.extend(['-DBLAS_LIBRARIES=%s' % blas_libs])
+        cmake_args.append('-DBUILD_SHARED_LIBS=ON')
+        cmake_args.append('-DBUILD_STATIC_LIBS=OFF')
+        cmake_args.append('-DCMAKE_INSTALL_LIBDIR=lib')
+        if platform.system() == 'Darwin':
+            cmake_args.append('-DCMAKE_SHARED_LINKER_FLAGS=-undefined dynamic_lookup')
+
+        cmake(*cmake_args)
+        make()
+        make("install")
+
+    @when('@exist')
     def install(self, spec, prefix):
         if os.getenv('ESSLROOT'):
             esslroot=os.environ['ESSLROOT']
