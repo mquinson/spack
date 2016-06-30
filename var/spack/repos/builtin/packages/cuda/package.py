9db5948da630329a7e0c83010da7c5cd4c4b2167
Florent Pruvost <florent.pruvost@inria.fr>
2016-06-30 17:13:39 +0200

    cuda: set CUDADIR env. var.

--- var/spack/repos/builtin/packages/cuda/package.py
+++ var/spack/repos/builtin/packages/cuda/package.py
@@ -19,8 +19,14 @@ class Cuda(Package):
             if os.path.isdir(cudaroot):
                 os.symlink(cudaroot+"/bin", prefix.bin)
                 os.symlink(cudaroot+"/include", prefix.include)
-                os.symlink(cudaroot+"/lib64", prefix.lib)
+                if os.path.isdir(cudaroot+"/lib64"):
+                    os.symlink(cudaroot+"/lib64", prefix.lib64)
+                if os.path.isdir(cudaroot+"/lib"):
+                    os.symlink(cudaroot+"/lib", prefix.lib)
             else:
                 raise RuntimeError(cudaroot+' directory does not exist.'+' Do you really have Nvidia CUDA installed in '+cudaroot+' ?')
         else:
             raise RuntimeError('CUDA_DIR environment variable does not exist. Please set CUDA_DIR to use Nvidia CUDA')
+
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        spack_env.set('CUDADIR', self.spec.prefix)
