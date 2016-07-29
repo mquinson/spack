e80424e769a95a30e391112c516cd14b3349f4b7
SYLVAND Guillaume <guillaume.sylvand@airbus.com>
2016-07-29 14:59:52 +0200

    Add package for 'anadel'

---
+++ var/spack/repos/builtin/packages/anadel/package.py
@@ -0,0 +1,70 @@
+from spack import *
+import spack
+import os
+import shutil
+import platform
+from subprocess import call
+
+class Anadel(Package):
+    """
+    A Finite Element Preprocessor.
+    Set the environment variable SOFTWARREEPO1 to get the versions.
+    """
+    pkg_dir  = spack.repo.dirname_for_package_name("fake")
+    homepage = pkg_dir
+    url      = "file:"+join_path(pkg_dir, "empty.tar.gz")
+
+    try:
+        repo=os.environ['SOFTWAREREPO1']
+        version('master', git=repo+'anadel.git', branch='master')
+        version('8.16.0',   git=repo+'anadel.git', branch='v8.16.0')
+    except KeyError:
+        pass
+
+    version('src')
+
+    variant('shared', default=True, description='Build ANADEL as a shared library')
+    variant('mpf', default=False, description='Link with mpf to have memory and performance profiler')
+
+    depends_on('cmake')
+    depends_on('mpf', when='+mpf')
+
+    if os.getenv("LOCAL_PATH"):
+        project_local_path = os.environ["LOCAL_PATH"] + "/anadel"
+
+    def build(self, spec, prefix):
+        with working_dir('spack-build', create=True):
+            cmake_args = [".."]
+            cmake_args.extend(std_cmake_args)
+            cmake_args.extend([
+                "-DCMAKE_COLOR_MAKEFILE:BOOL=ON",
+                "-DINSTALL_DATA_DIR:PATH=share",
+                "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"])
+
+            if spec.satisfies('%gcc'):
+                cmake_args.extend(["-DCMAKE_BUILD_TYPE=Debug",
+                                   "-DCMAKE_C_FLAGS=-fopenmp -D_GNU_SOURCE -pthread",
+                                   '-DCMAKE_C_FLAGS_DEBUG=-g -fopenmp -D_GNU_SOURCE -pthread',
+                                   '-DCMAKE_CXX_FLAGS=-pthread -fopenmp',
+                                   '-DCMAKE_CXX_FLAGS_DEBUG=-g -fopenmp -D_GNU_SOURCE -pthread',
+                                   '-DCMAKE_Fortran_FLAGS=-pthread -fopenmp',
+                                   '-DCMAKE_Fortran_FLAGS_DEBUG=-g -fopenmp -pthread'])
+
+            if spec.satisfies('+shared'):
+                cmake_args.extend(['-DBUILD_SHARED_LIBS=ON'])
+            else:
+                cmake_args.extend(['-DBUILD_SHARED_LIBS=OFF'])
+
+            if spec.satisfies('+mpf'):
+                mpf = spec['mpf'].prefix
+                cmake_args.extend(["-DMPF_DIR=%s/CMake" % mpf.share])
+
+            cmake(*cmake_args)
+            make()
+            make("install")
+
+    def install(self, spec, prefix):
+        if self.spec.satisfies('@src') and os.path.exists('spack-build'):
+            shutil.rmtree('spack-build')
+
+        self.build(spec,prefix)
