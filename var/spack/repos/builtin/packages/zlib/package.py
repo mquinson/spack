5b3ba507c5dfdfee721404f619cbd53e8685f9fe
gmarait <gilles.marait@gmail.com>
2017-01-20 13:44:29 +0100

    Fix md5 zlib

--- var/spack/repos/builtin/packages/zlib/package.py
+++ var/spack/repos/builtin/packages/zlib/package.py
@@ -33,7 +33,7 @@ class Zlib(Package):
     homepage = "http://zlib.net"
     url      = "http://www.zlib.net/zlib-1.2.11.tar.gz"
 
-    version('1.2.11', 'd9794246f853d15ce0fcbf79b9a3cf13')
+    version('1.2.11', '1c9f62f0778697a09d36121ead88e08e')
 
     def install(self, spec, prefix):
         configure("--prefix=%s" % prefix)
