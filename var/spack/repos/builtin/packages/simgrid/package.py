3aaf7a6d2eaa237bdef05d354e94cb93213658e2
Samuel Thibault <samuel.thibault@ens-lyon.org>
2017-05-04 10:57:05 +0200

    Drop simgrid version 3.10, we don't test it

--- var/spack/repos/builtin/packages/simgrid/package.py
+++ var/spack/repos/builtin/packages/simgrid/package.py
@@ -17,8 +17,6 @@ class Simgrid(Package):
             url='http://gforge.inria.fr/frs/download.php/file/35215/SimGrid-3.12.tar.gz')
     version('3.11', '358ed81042bd283348604eb1beb80224',
             url='http://gforge.inria.fr/frs/download.php/file/33683/SimGrid-3.11.tar.gz')
-    version('3.10', 'a345ad07e37539d54390f817b7271de7',
-            url='http://gforge.inria.fr/frs/download.php/file/33124/SimGrid-3.10.tar.gz')
     version('master', git='https://scm.gforge.inria.fr/anonscm/git/simgrid/simgrid.git')
     version('starpumpi', git='https://scm.gforge.inria.fr/anonscm/git/simgrid/simgrid.git', branch='starpumpi')
 
