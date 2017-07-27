c80b3d767e2697e43799ab24f0d303d4f39e541b
Florent Pruvost <florent.pruvost@inria.fr>
2017-07-27 09:55:51 +0200

    use cmake and openmpi as external packages to avoid having trooble compiling them on CI

---
+++ share/spack/qa/create_packages_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p ~/.spack/
+cat > ~/.spack/packages.yaml <<EOF
+packages:
+  cmake:
+    paths:
+      cmake@2.8.12%gcc@4.8.4 arch=linux-ubuntu14-x86_64: /usr
+    buildable: False
+  openmpi:
+    paths:
+      openmpi@1.6.5%gcc@4.8.4 arch=linux-ubuntu14-x86_64: /usr
+    buildable: False
+EOF
