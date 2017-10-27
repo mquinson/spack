dc479152360b84b01909d8861a6a3ca84971f356
MARAIT Gilles <gilles.marait@gmail.fr>
2017-10-27 18:19:13 +0200

    Update py-maphys

--- var/spack/repos/builtin/packages/py-maphys/package.py
+++ var/spack/repos/builtin/packages/py-maphys/package.py
@@ -17,16 +17,6 @@ class PyMaphys(Package):
     depends_on("py-mpi4py")
     depends_on("py-numpy")
     depends_on("py-scipy")
-    depends_on("swig")
-
-    pypath = ''
 
     def install(self, spec, prefix):
-        for d in ["lib", "examples"]:
-            target = os.path.join(prefix, d)
-            shutil.copytree(d, target)
-        shutil.copy("README.org", prefix)
-
-    def setup_environment(self, spack_env, run_env):
-        pypath = os.path.join(self.prefix, 'lib')
-        run_env.prepend_path('PYTHONPATH', pypath)
+        python('setup.py', 'install', '--prefix=%s' % prefix)
